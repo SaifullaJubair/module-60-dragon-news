e8981bb77371fdaabd208bfea2cb55377cc9c23d
SaifullaJubair <saifullajubair6@gmail.com>
2022-10-21 07:23:16 +0600

    create layout ,folder structure pages wise

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Header = () => {
+const News = () => {
    return (
       <div>
-
+         <h2>This is news</h2>
       </div>
    );
 };
 
-export default Header;
+export default News;
