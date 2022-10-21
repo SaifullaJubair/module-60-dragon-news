b179b84063d1c0f908b83794d01fafd761ae9e0a
SaifullaJubair <saifullajubair6@gmail.com>
2022-10-21 10:36:18 +0600

    60-7 carousel react-icon

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch, FaLinkedin } from 'react-icons/fa';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
+
 
 const RightSideNav = () => {
    return (
       <div>
-         <h2>Right side nav</h2>
+         <div>
+            <ButtonGroup vertical>
+               <Button className='mb-2' variant='outline-dark'>
+                  <FcGoogle /> Login Via Google
+               </Button>
+               <Button className='mb-2' variant='outline-dark'>
+                  <FaGithub /> Login Via Github
+               </Button>
+            </ButtonGroup>
+         </div>
+
+         <div>
+            <ListGroup>
+               <ListGroup.Item>
+                  <FaFacebook></FaFacebook> Facebook
+               </ListGroup.Item>
+               <ListGroup.Item>
+                  <FaTwitter /> Twitter
+               </ListGroup.Item>
+               <ListGroup.Item>
+                  <FaTwitch /> Twitch
+               </ListGroup.Item>
+               <ListGroup.Item>
+                  <FaWhatsapp /> Whatsapp
+               </ListGroup.Item>
+               <ListGroup.Item>
+                  <FaLinkedin /> Linkedin
+               </ListGroup.Item>
+            </ListGroup>
+         </div>
+         <div>
+            <BrandCarousel></BrandCarousel>
+         </div>
       </div>
    );
 };
