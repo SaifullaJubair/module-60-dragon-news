e89c903362d015f4965595f9a9489ad0b09cdf4e
SaifullaJubair <saifullajubair6@gmail.com>
2022-10-21 23:32:23 +0600

    61-4 card img style

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+import { BsFillBookmarkStarFill, BsFillShareFill } from "react-icons/bs";
 
 const NewsSummaryCard = ({ news }) => {
+   console.log(news);
+   const { _id, title, author, details, image_url, total_view } = news
    return (
-      <div>
-         {news.title}
-      </div>
+      <Card className="mb-5">
+         <Card.Header className='d-flex justify-content-between align-items-center'>
+            <div className='d-flex align-items-center'>
+               <Image
+                  className='me-2'
+                  roundedCircle
+                  src={author.img}
+                  style={{ height: '60px' }}
+               ></Image>
+               <div>
+                  <p>
+                     {author.name}
+                  </p>
+
+               </div>
+            </div>
+            <div>
+               <BsFillBookmarkStarFill />
+               <BsFillShareFill />
+            </div>
+         </Card.Header>
+         <Card.Body>
+            <Card.Title>
+               {title}
+            </Card.Title>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Text>
+               {
+                  details.length > 250 ?
+                     <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link> </p>
+                     :
+                     <p>{details}</p>
+               }
+            </Card.Text>
+
+         </Card.Body>
+         <Card.Footer className="text-muted">
+            <p>
+               {author.published_date}
+            </p>
+         </Card.Footer>
+      </Card>
    );
 };
 
